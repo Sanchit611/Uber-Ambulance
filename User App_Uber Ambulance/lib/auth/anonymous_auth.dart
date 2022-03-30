ccbd1b4bd574f591e90d71e086f514046a407cb4
Sanchit Narang <79566726+Sanchit611@users.noreply.github.com>
2022-03-30 16:03:33 +0530

    Update anonymous_auth.dart

--- User App_Uber Ambulance/lib/auth/anonymous_auth.dart
+++ User App_Uber Ambulance/lib/auth/anonymous_auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+
 import 'auth_util.dart';
 
 Future<User> signInAnonymously(BuildContext context) async {
