48c3dbee8f06d72dfc1ab27ed213d77c6c134626
Sanchit Narang <79566726+Sanchit611@users.noreply.github.com>
2022-03-30 16:04:27 +0530

    Update storage.dart

--- User App_Uber Ambulance/lib/backend/firebase_storage/storage.dart
+++ User App_Uber Ambulance/lib/backend/firebase_storage/storage.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:mime_type/mime_type.dart';
 
+
 Future<String> uploadData(String path, Uint8List data) async {
   final storageRef = FirebaseStorage.instance.ref().child(path);
   final metadata = SettableMetadata(contentType: mime(path));
